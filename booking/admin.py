5d70d88c7dbee6d5ecbbd08f6810e3eedc4a76fd
sahil <sahilsanghvi26@gmail.com>
2025-09-20 10:09:29 +0530

    Login implementation completed

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib import admin
+from .models import Booking, Show, Seat, BookedSeat,City,Theatre,UserProfile,Movie
 
-# Register your models here.
+admin.site.register(City)
+admin.site.register(UserProfile)
+admin.site.register(Movie)
+admin.site.register(Theatre)
+admin.site.register(Show)
+admin.site.register(Seat)
+admin.site.register(Booking)
+admin.site.register(BookedSeat)
+
